101cb8072af384dc4e2c5a1f3a583eae735c4932
Chris Klosowski <cklosowski@gmail.com>
2014-11-02 21:29:13 -0700

    Make it so

---
+++ yo-dawg-post-meta.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: Yo Dawg Post Meta
+Plugin URI: https://www.filament-studios.com
+Description: Adds a metabox to a post, that shows the post meta
+Version: 1.0
+Author: Chris Klosowski
+Author URI: http://www.kungfugrep.com
+License: GPLv2 or later
+*/
+
+/**
+ * Register the metaboxes for Post Promoter Pro
+ * @return void
+ */
+function ydmb_register_meta_boxes() {
+	global $post;
+
+	$post_types = apply_filters( 'ydpm_allowed_post_types', array( 'post', 'page' ) );
+
+	if ( !isset( $post_types ) || !is_array( $post_types ) ) {
+		return;
+	}
+
+	foreach ( $post_types as $post_type ) {
+		add_meta_box( 'ydmb_meta', 'Yo Dawg, I heard you like post meta, so I put your post meta in a registerd metabox', 'ydmb_metabox_callback', $post_type, 'normal', 'low' );
+	}
+}
+add_action( 'add_meta_boxes', 'ydmb_register_meta_boxes', 10 );
+
+/**
+ * Display the Metabox for Post Promoter Pro
+ * @return void
+ */
+function ydmb_metabox_callback() {
+	global $post;
+	echo '<div style="overlfow:auto">';
+	$post_meta = get_metadata( 'post', $post->ID );
+	echo '<pre style="overflow: auto;word-wrap: break-word;">';
+	foreach ( $post_meta as $key => $value ) {
+		echo $key . ' => ' . $value[0] . "\n";
+	}
+	echo '</pre>';
+	echo '</div>';
+}
+
+
